f72423dced2426f62ee331e2d1c9f9d3e77560fb
changwoo <ep6tri@hotmail.com>
2021-03-14 14:42:37 +0900

    Submodule loading fix

--- src/Modules/SubModuleImpl.php
+++ src/Modules/SubModuleImpl.php
@@ -4,21 +4,41 @@
 namespace Naran\Axis\Modules;
 
 
+use Closure;
+
 trait SubModuleImpl
 {
     protected array $modules = [];
 
-    public function __get(string $name): ?Module
+    public function __get(string $name)
     {
-        return $this->modules[$name] ?? null;
+        $module = $this->modules[$name] ?? null;
+
+        if ($module instanceof Closure) {
+            $module = call_user_func($module, $this);
+            if (is_string($module) && class_exists($module)) {
+                $container = $this->getContainer();
+                $container->singletonIf($module);
+                $this->modules[$name] = $module = $container->get($module);
+            } else {
+                $this->modules[$name] = $module;
+            }
+        }
+
+        return $module;
     }
 
     protected function initSubModules(array $modules)
     {
         $container = $this->getContainer();
+
         foreach ($modules as $name => $module) {
-            $container->singletonIf($module);
-            $this->modules[$name] = $container->get($module);
+            if (is_string($module)) {
+                $container->singletonIf($module);
+                $this->modules[$name] = $container->get($module);
+            } else {
+                $this->modules[$name] = $module;
+            }
         }
     }
 }
